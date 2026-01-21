1df4bcc7173380406a642cc9f60d654e5eb0445f
Femi3211 <femi.havziu123@gmail.com>
2026-01-21 15:38:00 +0100

    added inital extracting support for Sybase(SAP IQ)

---
+++ source/src/main/java/com/adataptivescale/rosetta/source/core/extractors/column/SAPIQColumnsExtractor.java
@@ -0,0 +1,58 @@
+/*
+ *  Copyright 2022 AdaptiveScale
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *            http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.adataptivescale.rosetta.source.core.extractors.column;
+
+import com.adaptivescale.rosetta.common.TranslationMatrix;
+import com.adaptivescale.rosetta.common.annotations.RosettaModule;
+import com.adaptivescale.rosetta.common.models.Column;
+import com.adaptivescale.rosetta.common.models.input.Connection;
+import com.adaptivescale.rosetta.common.types.RosettaModuleTypes;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+@RosettaModule(
+        name = "sapiq",
+        type = RosettaModuleTypes.COLUMN_EXTRACTOR
+)
+public class SAPIQColumnsExtractor extends ColumnsExtractor{
+    public SAPIQColumnsExtractor(Connection connection) {
+        super(connection);
+    }
+
+    @Override
+    protected void extract(ResultSet resultSet, Column column) throws SQLException {
+        column.setName(resultSet.getString("COLUMN_NAME"));
+
+        String columnType = String.valueOf(resultSet.getString("TYPE_NAME"));
+        column.setTypeName(TranslationMatrix.getInstance().findBySourceTypeAndSourceColumnType("sapiq", columnType));
+
+        // SAP IQ/Sybase may return "YES"/"NO" for IS_NULLABLE instead of boolean
+        String nullableStr = resultSet.getString("IS_NULLABLE");
+        if (nullableStr != null && (nullableStr.equalsIgnoreCase("YES") || nullableStr.equalsIgnoreCase("NO"))) {
+            column.setNullable("YES".equalsIgnoreCase(nullableStr));
+        } else {
+            column.setNullable(resultSet.getBoolean("IS_NULLABLE"));
+        }
+        
+        column.setColumnDisplaySize(resultSet.getInt("COLUMN_SIZE"));
+        column.setScale(resultSet.getInt("DECIMAL_DIGITS"));
+        column.setPrecision(resultSet.getInt("COLUMN_SIZE"));
+    }
+}
+
